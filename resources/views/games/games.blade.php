d156c9ff0aefd1e52ea8ce131a17ae9a1c595d41
Yago Santos <yago.profissional.dev@outlook.com>
2024-05-24 14:14:09 -0300

    Update.24/05/24

--- resources/views/games/games.blade.php
+++ resources/views/games/games.blade.php
@@ -6,6 +6,12 @@
 
 <h1>Escolha e se divirta!</h1>
 
+    <div class="cards-container">
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+    </div>
+
 @foreach($games as $game) <!-- Loop que itera sobre a variável $games -->
     <p>{{ $game->name }}</p> <!-- Exibe o nome de cada jogo -->
 @endforeach
